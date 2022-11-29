5221ebb5f5cecc9fc4dd0522783a1ae49c02c59a
sagebeme <eudicas5k@gmail.com>
2022-11-29 07:28:06 -0800

    correcting output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i is not 101 and i is not 113:
-        print("{:c}".format(i), end="")
+for character in range(97, 123):
+    if (character != 101 and character != 113):
+        print("{:c}".format(character), end='')
