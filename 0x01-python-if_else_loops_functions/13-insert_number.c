baeb40a28bd02e8b15d124973a8378d82c6dbaa4
sagebeme <eudicas5k@gmail.com>
2022-11-30 20:49:43 +0300

    inserting a number in a sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,34 +1,53 @@
 #include "lists.h"
 
 /**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the new node.
+ * insert_node - this function inserts a node in a sorted manner
+ * @head: the head node
+ * @number: the number to be inserted
+ * Description: returns a pointer to the new node
+ * Return: returns a pointer to the new node
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+	int flag = 0;
+	listint_t *new_node, *current;
+	listint_t *next;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (head == NULL)
 		return (NULL);
-	new->n = number;
-
-	if (node == NULL || node->n >= number)
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number, new_node->next = NULL;
+	if (*head == NULL)
 	{
-		new->next = node;
-		*head = new;
-		return (new);
+		*head = new_node;
+		return (*head);
 	}
-
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
+	current = *head;
+	if (number <= current->n)
+	{
+		new_node->next = current, *head = new_node;
+		return (*head);
+	}
+	if (number > current->n && !current->next)
+	{
+		current->next = new_node;
+		return (new_node);
+	}
+	next = current->next;
+	while (current)
+	{
+		if (!next)
+			current->next = new_node, flag = 1;
+		else if (next->n == number)
+			current->next = new_node, new_node->next = next, flag = 1;
+		else if (next->n > number && current->n < number)
+			current->next = new_node, new_node->next = next, flag = 1;
+		if (flag)
+			break;
+		next = next->next, current = current->next;
+	}
+	return (new_node);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,25 +1,26 @@
-#ifndef Lists_H
-#define Lists_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * description: singly linked list node structure
- * for Alx project
-*/
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
-} listint_s;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
 listint_t *insert_node(listint_t **head, int number);
 
-#endif
+#endif /* LISTS_H */
