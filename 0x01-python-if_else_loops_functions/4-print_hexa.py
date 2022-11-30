7a0962893552b47c3447e41c2efd9a934386a940
sagebeme <eudicas5k@gmail.com>
2022-11-30 12:57:41 +0300

    changing the code

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-for num in range(0, 99):
-    print("{:d} = 0x{:x}".format(num, num))
-'''!/bin/bash/python3
+#!/bin/bash/python3
 for i in range(0,99):
     print("{} = {}".format(i, hex(i)))
-'''
