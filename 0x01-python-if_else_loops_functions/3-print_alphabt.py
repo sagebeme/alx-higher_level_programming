f617ee6b6f92c57d1a34fc5179a2b51c10079b4a
sagebeme <eudicas5k@gmail.com>
2022-11-30 11:47:27 +0300

    changing whole code to use letters insted of numbers

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for all_letters in range(ord('a'), ord('z')+1):
     letter = chr(all_letters)
     if letter not in "qe":
-        print(letter, end="")
+        print("{:s}".format(letter), end="")
