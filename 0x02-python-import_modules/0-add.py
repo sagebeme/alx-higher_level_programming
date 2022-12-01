b76ab77413fd1d4da1155774e3a638696ae94ead
sagebeme <eudicas5k@gmail.com>
2022-12-01 13:54:41 +0300

    removing the 1st comment to allow scripts execution

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-'''allows you to run shell scripts'''
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
     from add_0 import add
@@ -7,4 +6,3 @@ if __name__ == "__main__":
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
