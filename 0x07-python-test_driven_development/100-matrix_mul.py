aeda133d153d3e4f9c12d5eb1b3e17ee802424e8
Sagebeme <eudicas5k@gmail.com>
2023-01-08 00:18:53 +0300

    multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # 100-matrix_mul.py
 # Sagebeme
+
 """Defines a matrix multiplication function."""
 
 
 def matrix_mul(m_a, m_b):
     """Multiply two matrices.
-
     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
