c8985686bc72f74791a0193738102669efb23659
sagebeme <eudicas5k@gmail.com>
2023-02-17 07:58:13 +0300

    create DB

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,4 @@
 -- Creates the database hbtn_0d_2 and with id and name
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
 CREATE TABLE
     IF NOT EXISTS `force_name`(
         `id`    INT,
