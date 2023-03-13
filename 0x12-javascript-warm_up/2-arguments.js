c77c7cecc1598ffa25b1eac7ad36c583d92b004e
sagebeme <eudicas5k@gmail.com>
2023-03-13 19:02:20 +0300

    argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 
-cont { argv } = require('process');
-if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else
+const { argv } = require('process');
+if (argv.length === 2) { console.log('No argument'); } 
+else if (argv.length === 3) { console.log('Argument found'); } 
+else {console.log('Argument found')}
