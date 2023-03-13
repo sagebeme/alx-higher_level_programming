f7ee1f852067e162974a18385e2eec7090334048
sagebeme <eudicas5k@gmail.com>
2023-03-13 23:15:17 +0300

    loops in Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
