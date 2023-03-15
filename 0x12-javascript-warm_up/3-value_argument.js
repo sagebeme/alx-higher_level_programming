5a4310f5652919c456e41e942cd7dfbf7a166904
sagebeme <eudicas5k@gmail.com>
2023-03-15 17:15:13 +0300

    changing to a readable code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+const argv = process.argv[2];
+if (argv){
+	console.log(argv);
+} else {
+	console.log("No Argument");
+}
