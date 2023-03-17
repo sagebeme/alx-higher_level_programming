310848a8d4aab1b0a905fd751310b4d23e0d3a7e
sagebeme <eudicas5k@gmail.com>
2023-03-17 13:34:48 +0300

    doc string module import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+
+"""
+Database -module import
+    """
+import sys
+import MySQLdb
+
 ''' lists all states from the database hbtn_0e_0_usa
     Usage: ./0-select_states.py   <mysql username>\
                                   <mysql password>\
                                   <database name>
 '''
-import sys
-import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
