47661b62304f50dd2b8d028d9d49389d971217ce
sagebeme <eudicas5k@gmail.com>
2023-03-20 00:59:49 +0300

    ensuring one doesn't execute unsafr code by using SQL querry injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""
-displays all values in the states table of
- hbtn_0e_0_usa where name matches the argument
-"""
 
-import sys
+"""module - ensure to remove mysql injection in code"""
 import MySQLdb
+from sys import argv
+
+""" The function below filters out code to ensure
+one does not add malicious code"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states""")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], database=argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name like BINARY"
+            "'{:s}' ORDER BY id ASC".format(argv[4].replace("'", "''")))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
