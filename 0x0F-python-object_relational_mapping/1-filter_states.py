73413455229e5170ffe43059397c5868ff0dea4c
sagebeme <eudicas5k@gmail.com>
2023-03-19 16:33:57 +0300

    rm local host

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
+    db = MySQLdb.connect(user=sys.argv[1],
             passwd=sys.argv[2],db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
