7edecab49daa0b564957909df4f4820267f48f6c
sagebeme <eudicas5k@gmail.com>
2023-03-20 01:01:01 +0300

    script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""
-lists all cities from the database hbtn_0e_4_usa
-"""
 
-import sys
+"""module - inner joins sql statement"""
 import MySQLdb
+from sys import argv
+
+""" a function to use inner joins in mysqldb"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT cities.id, cities.name, states.name
-                FROM cities
-                JOIN states
-                ON cities.state_id = states.id
-                ORDER BY cities.id ASC""")
-    [print(city) for city in c.fetchall()]
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], database=argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+            """SELECT cities.id, cities.name, states.name FROM cities INNER
+            JOIN states ON cities.state_id =  states.id""")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
