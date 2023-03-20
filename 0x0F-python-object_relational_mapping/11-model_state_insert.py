ec5ae046d17df8fcb60f247050bc96d4b9f229b6
sagebeme <eudicas5k@gmail.com>
2023-03-20 11:58:24 +0300

    script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""
- adds the State object "Louisiana" to the database hbtn_0e_6_usa
-"""
+""" slqalchmy query """
 
-import sys
-from unicodedata import name
-from sqlalchemy import create_engine
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+import sys
+from sqlalchemy import create_engine, desc
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
-
-    obj = State(name="Louisiana")
-    session.add(obj)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
     session.commit()
-    print(obj.id)
+    results = session.query(State.id).order_by(State.id.desc()).first()
+    if (results):
+        print(results[0])
+    session.close()
