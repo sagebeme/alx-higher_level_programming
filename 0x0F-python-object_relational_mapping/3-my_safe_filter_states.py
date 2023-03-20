1b445928852d439d1614a4203735c2698917e48a
sagebeme <eudicas5k@gmail.com>
2023-03-20 11:32:18 +0300

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
 
-"""module - ensure to remove mysql injection in code"""
+'''
+lists all states in the database hbtn_0e_0_usa
+'''
+import sys
 import MySQLdb
-from sys import argv
 
-""" The function below filters out code to ensure
-one does not add malicious code"""
 
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], database=argv[3])
-    cursor = db.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name like BINARY"
-            "'{:s}' ORDER BY id ASC".format(argv[4].replace("'", "''")))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    db.close()
+    try:
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("connection error")
+    try:
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY\
+        states.id", (sys.argv[4],))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error:
+        print("Failed")
+    cur.close()
+    connection.close()
