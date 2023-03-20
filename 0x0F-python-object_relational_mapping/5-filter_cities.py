55cd923145a1b371c6d15f87d1ee44297ec73e94
sagebeme <eudicas5k@gmail.com>
2023-03-20 11:32:23 +0300

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
-"""
-lists all cities of that state, using the database hbtn_0e_4_usa
-"""
+
+'''
+lists all states in the database hbtn_0e_0_usa
+'''
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM cities
-                INNER JOIN states
-                ON cities.state_id = states.id
-                ORDER BY cities.id""")
-    print(", ".join([city[2]
-                     for city in c.fetchall()
-                     if city[4] == sys.argv[4]]))
+    try:
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("connection error")
+    try:
+        cur = connection.cursor()
+        cur.execute("SELECT cities.name FROM cities\
+        INNER JOIN states\
+        ON cities.state_id = states.id\
+        WHERE states.name = %s\
+        ORDER BY cities.id", (sys.argv[4],))
+        rows = cur.fetchall()
+        cities = []
+        for row in rows:
+            cities.append(row[0])
+        print(", ".join(cities))
+    except MySQLdb.Error:
+        print("Failed")
+    cur.close()
+    connection.close()
