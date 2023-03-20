fb40e3cf4b0c1518807d59d5abb772671fa6e668
sagebeme <eudicas5k@gmail.com>
2023-03-20 11:32:28 +0300

    script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""
-Prints the first State object from the database hbtn_0e_6_usa.
-Usage: ./8-model_state_fetch_first.py <mysql username> /
-                                      <mysql password> /
-                                      <database name>
-"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+""" slqalchmy query """
 
-# FUNCTIONALITY
-# 1. Importing the necessary modules
-# 2. Creating the engine and session
-# 3. Querying the database for the first State object
-# 4. Printing the id and name of the first State object
-# 5. If there is no State object, print “Nothing”
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import (create_engine)
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
-
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if (state):
+        print(f"{state.id}: {state.name}")
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("Nothing")
+    session.close()
