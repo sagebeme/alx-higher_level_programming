c5e3042270297eb871ed0a4efa893c03ee52c1b8
sagebeme <eudicas5k@gmail.com>
2023-03-24 16:12:00 +0300

    lists all obj with letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,15 +9,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'
-            .format(argv[1],argv[2],argv[3]),
-            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id)\
-                                 .filter(State.name.like("%a%")).all()
-    for res in result:
-        print("{}: {}".format(res.id, res.name))
-    session.close()
+
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
