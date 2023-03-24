639b183a3efd8c4e3ddef13f6e7822700761bf75
sagebeme <eudicas5k@gmail.com>
2023-03-24 16:03:44 +0300

    scripts that lists

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,18 +6,8 @@ lists all states in the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], database=argv[3])
-    cursor = db.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name like BINARY"
-            "'{:s}' ORDER BY id ASC".format(argv[4].replace("'", "''")))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states""")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
