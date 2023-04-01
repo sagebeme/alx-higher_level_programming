350646e4044459929023b3991b734bcbbfaa3609
sagebeme <eudicas5k@gmail.com>
2023-04-01 19:16:35 +0300

    pycodestyle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -13,4 +13,3 @@ if __name__ == "__main__":
     quest = urllib.request.Request(argv[1])
     with urllib.request.urlopen(quest) as response:
         print(dict(response.headers).get("X-Request-Id"))
-
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Takes in a URL and an email, sends a POST to the requested
+URL with the email as the parameter and displays the response
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {'email': sys.argv[2]}
+    params = urllib.parse.urlencode(val).encode('ascii')
+
+    req = urllib.req.Request(url, params)
+    with urllib.req.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
