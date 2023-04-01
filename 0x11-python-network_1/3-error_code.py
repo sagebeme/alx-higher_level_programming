3a409a4adad439b7bcb55171009ae70e3295671f
sagebeme <eudicas5k@gmail.com>
2023-04-01 19:42:25 +0300

    displays error message or body

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,7 +11,7 @@ from sys import argv
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("utf-8"))
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.code)
