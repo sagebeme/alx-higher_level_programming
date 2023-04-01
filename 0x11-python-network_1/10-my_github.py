b6434685a14f21a204dfc65702e2c6205b0b78bf
sagebeme <eudicas5k@gmail.com>
2023-04-01 20:03:57 +0300

    links git hub with my credentials and works with github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes my Github creds (username & password)
+and uses the Github API to display my ID
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
